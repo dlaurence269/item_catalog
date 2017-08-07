dc77b10aa212ad65fc657a2db2be00e15007d115
Daniel Laurence <daniellaurence269@gmail.com>
2017-08-07 13:21:37 -0400

    Create README.md, main.py, database_setup.py, and the templates folder.

---
+++ database_setup.py
@@ -0,0 +1,64 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(500))
+    price = Column(String(8))
+    ibu = Column(String(8))
+    abv = Column(String(8))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'ibu': self.ibu,
+            'abv': self.abv
+        }
+
+
+engine = create_engine('sqlite:///beers.db')
+
+
+Base.metadata.create_all(engine)
---
+++ main.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, redirect, jsonify, url_for
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, Item
+from flask import session as login_session
+
+
+app = Flask(__name__)
+
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///beers.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# App Routing
+
+# 1. Landing
+@app.route('/') # GET - Landing page, directs routes to /beers
+@app.route('/beers') # GET - List of all beers
+def showBeers():
+    return "This is the beers landing page."
+
+# 2. Item in detail
+@app.route('/beers/<int:item_id>') # GET - See a specific item in detail
+def showItemInDetail():
+    return "This is a specific items' detailed page."
+
+# 3. New
+@app.route('/beers/new') # GET - View to create a new item # POST - Create a new item
+def newBeer():
+    return "This is the create new item page."
+
+# 4. Edit
+@app.route('/beers/<int:item_id>/edit') # GET - View to edit a specific item # POST - Update a specific item
+def editBeer():
+    return "This is the edit a specific item page."
+
+# 4. Delete
+@app.route('/beers/<int:item_id>/delete') # GET - View to delete a specific item (only if no popup) # POST - Delete a specific item
+def deleteBeer():
+    return "This is the delete a specific item page."
+
+# 6. Login
+@app.route('/login') # GET - VView to login or signup # POST - Login via third party API
+def login():
+    return "This is the login page."
+
+# 7. JSON all
+@app.route('/beers/json') # GET - View JSON for all items (in alphabetical order)
+def showJSONAll():
+    return "This is the view all beers in JSON page."
+
+# 8. JSON specific item
+@app.route('/beers/<int:item_id>/json') # GET - View JSON for a specific item
+def showJSONItem():
+    return "This is the view JSON for a specific item page."
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
