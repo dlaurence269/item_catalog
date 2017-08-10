7a567b962819d62b8aab94f0512c4c6a0c0a7ca5
Daniel Laurence <daniellaurence269@gmail.com>
2017-08-09 20:15:25 -0400

    Populated database successfully

--- populate_beers.py
+++ populate_beers.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Restaurant, Base, MenuItem, User
+from database_setup import Category, Base, Item, User
 
-engine = create_engine('sqlite:///beers.db')
+engine = create_engine('sqlite:///beer_catalog.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
