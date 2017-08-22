bc3d0dc82bf61e9d9d21beccfac6a72f9c033f38
Daniel Laurence <daniellaurence269@gmail.com>
2017-08-22 15:26:26 -0400

    Completed newBeer

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ session = DBSession()
 # App Routing
 
 # 1. Landing
+# GET - All Beers
 @app.route('/') # GET - Landing page, directs routes to /beer
 @app.route('/beers') # GET - List of all beer
 def showAllBeers():
@@ -36,34 +37,37 @@ def showAllBeers():
     # do something with the beers list
     return render_template('showAllBeers.html', categories=categories, items=items, Item=Item)
 
-# 2. Show specific beer 
-@app.route('/beers/<int:item_id>') # GET - See a specific item in detail
+# 2. Show specific beer
+# GET - See a specific item in detail
+@app.route('/beers/<int:item_id>')
 def showSpecificBeer(item_id):
     item = session.query(Item).filter_by(id=item_id).one()
     return render_template('showSpecificBeer.html', item=item)
 
 # 3. New
-# GET - View to create a new item # POST - Create a new item
+# GET - View to create a new item
+# POST - Create a new item
 @app.route('/beers/new', methods=['GET', 'POST'])
 def newBeer():
+    categories = session.query(Category).order_by(Category.id).all()
     if request.method == 'POST':
         newItem = Item(name=request.form['name'], description=request.form['description'], 
                         picture_path=request.form['picture_path'], price=request.form['price'], 
-                        ibu=request.form['ibu'], abv=request.form['abv']#,
-                        #category=request.form['category']
-                        )
+                        ibu=request.form['ibu'], abv=request.form['abv'],
+                        category_id=request.form['category_id'])
         session.add(newItem)
         session.commit()
         return redirect(url_for('showAllBeers'))
     else:
-        return render_template('newBeer.html')
+        return render_template('newBeer.html', categories=categories)
 
 # 4. Edit
-# Add methods=['GET', 'POST']
-@app.route('/beers/<int:item_id>/edit') # GET - View to edit a specific item # POST - Update a specific item
+# GET - View to edit a specific item
+# POST - Update a specific item
+@app.route('/beers/<int:item_id>/edit', methods=['GET', 'POST'])
 def editBeer(item_id):
     editedItem = session.query(Item).filter_by(id=item_id).one()
-    # item = session.query(Item).filter_by(id=item_id).one()
+    categories = session.query(Category).order_by(Category.id).all()
     if request.method == 'POST':
         if request.form['name']:
             editedItem.name = request.form['name']
@@ -77,16 +81,18 @@ def editBeer(item_id):
             editedItem.ibu = request.form['ibu']
         if request.form['abv']:
             editedItem.abv = request.form['abv']
-        if request.form['category']:
-            editedItem.category = request.form['category']
+        if request.form['category_id']:
+            editedItem.category_id = request.form['category_id']
         session.add(editedItem)
         session.commit()
         return redirect(url_for('showAllBeers'))
     else:
-        return render_template('editBeer.html', item=editedItem)
+        print (editedItem.category_id)
+        return render_template('editBeer.html', item=editedItem, categories=categories)
 
 # 5. Delete
-# GET - View to delete a specific item (only if no popup) # POST - Delete a specific item
+# GET - View to delete a specific item (only if no popup)
+# POST - Delete a specific item
 @app.route('/beers/<int:item_id>/delete', methods=['GET', 'POST'])
 def deleteBeer(item_id):
     itemToDelete = session.query(Item).filter_by(id=item_id).one()
